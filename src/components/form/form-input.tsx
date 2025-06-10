8593e25616e6c0b0a6eceeec991375f1d7e7d799
royman.dev <royman.dev@gmail.com>
2025-06-10 13:43:48 +0700

    feat: adjust form input style

--- src/components/form/form-input.tsx
+++ src/components/form/form-input.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/tailwind"
+import { useState } from "react"
 import { ControllerProps, FieldPath, FieldValues } from "react-hook-form"
 import BaseLabel from "../base/base-label"
 import {
@@ -23,37 +25,50 @@ const FormInput = <
     InputProps,
     "type" | "autoFocus" | "accept" | "placeholder" | "className"
   >) => {
-  return (
-    <FormField
-      control={control}
-      name={name}
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      defaultValue={"" as any}
-      render={({ field }) => (
-        <FormItem>
-          <BaseLabel asChild>
-            <FormLabel>{label}</FormLabel>
-          </BaseLabel>
-          <FormControl>
-            <Input
-              autoComplete="off"
-              {...field}
-              {...rest}
-              value={rest.type === "file" ? field.value?.fileName : field.value}
-              onChange={(e) => {
-                if (rest.type === "file") {
-                  field.onChange(e.target.files?.[0])
-                  return
+  const [focus, setFocus] = useState(false)
+
+    return (
+      <FormField
+        control={control}
+        name={name}
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        defaultValue={"" as any}
+        render={({ field }) => (
+          <FormItem className="relative pt-4">
+            <BaseLabel asChild>
+              <FormLabel
+                className={cn(
+                  "absolute top-8 focus:top-0 transition-[top]",
+                  (field.value || focus) && "top-0",
+                )}
+              >
+                {label}
+              </FormLabel>
+            </BaseLabel>
+            <FormControl>
+              <Input
+                autoComplete="off"
+                {...field}
+                {...rest}
+                value={
+                  rest.type === "file" ? field.value?.fileName : field.value
                 }
-                field.onChange(e.target.value)
-              }}
-            />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  )
+                onChange={(e) => {
+                  if (rest.type === "file") {
+                    field.onChange(e.target.files?.[0])
+                    return
+                  }
+                  field.onChange(e.target.value)
+                }}
+                onFocus={() => setFocus(true)}
+                onBlur={() => setFocus(false)}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    )
 }
 
 export default FormInput
