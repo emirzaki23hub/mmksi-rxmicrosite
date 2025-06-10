8ba3f0e42eb0dfe51152004a0060f8ae25170a1d
royman.dev <royman.dev@gmail.com>
2025-06-10 11:17:39 +0700

    feat: change button style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-white underline-offset-4 hover:underline",
       },
       size: {
         default: "h-[60px] px-7 py-0 has-[>svg]:px-3",
